71487e0b04e19fc01912a9bb9c9cb40554cc0a9b
Ernesto Serrano <info@ernesto.es>
2026-07-14 19:58:45 +0100

    Remove duration from legacy PHP result

--- packages/php/src/RouteResult.php
+++ packages/php/src/RouteResult.php
@@ -1,2 +1,13 @@
-<?php declare(strict_types=1); namespace AteEducacion\CanariasRouteMatrix;
-final readonly class RouteResult { public function __construct(public int $distanceMeters, public int $durationSeconds) {} }
+<?php
+
+declare(strict_types=1);
+
+namespace AteEducacion\CanariasRouteMatrix;
+
+/** @deprecated Use DistanceResult. */
+final readonly class RouteResult
+{
+    public function __construct(public int $distanceMeters)
+    {
+    }
+}
