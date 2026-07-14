30444a60fda877ae1d9353a14391ef9abd8b2c52
Ernesto Serrano <info@ernesto.es>
2026-07-14 19:58:01 +0100

    Type the distance-only JavaScript API

--- packages/javascript/src/index.d.ts
+++ packages/javascript/src/index.d.ts
@@ -1,16 +1,21 @@
 export interface LoadOptions {
-  binaryUrl: string;
+  dataUrl?: string;
+  binaryUrl?: string;
   centersUrl: string;
 }
-export interface RouteResult {
+
+export interface DistanceResult {
   readonly distanceMeters: number;
-  readonly durationSeconds: number;
 }
-export declare class RouteMatrix {
+
+export declare class DistanceMatrix {
   constructor(buffer: ArrayBuffer, centers?: readonly object[]);
-  static load(options: LoadOptions): Promise<RouteMatrix>;
-  getRoute(origin: string, destination: string): RouteResult;
+  static load(options: LoadOptions): Promise<DistanceMatrix>;
+  getDistance(origin: string, destination: string): DistanceResult;
+  getRoute(origin: string, destination: string): DistanceResult;
 }
+
+export declare const RouteMatrix: typeof DistanceMatrix;
 export declare class InvalidFormatError extends Error {}
 export declare class UnknownCenterError extends Error {}
 export declare class CrossIslandRouteError extends Error {}
